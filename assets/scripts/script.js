381c25d48f75a65a315ef59d4896621b5aaf79ff
Ross Espley <itzrossyo@gmail.com>
2023-05-30 20:29:21 +0100

    added contact form also Feat:javascript to send email apond completing form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,3 +5,18 @@ const dropDownMenu = document.querySelector(".dropdown-menu");
 toggleBtn.onclick = function () {
   dropDownMenu.classList.toggle("open");
 };
+
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+            event.preventDefault();
+            var firstName = document.getElementById("first-name").value;
+            var lastName = document.getElementById("last-name").value;
+            var message = document.getElementById("message").value;
+            var contactMethod = document.getElementById("contact-method").value;
+            var email = "rossespley@gmail.com";
+
+            var confirmationMessage =
+              "Sent to " + email + "\n\n" + "First Name: " + firstName + "\n" + "Last Name: " + lastName + "\n" + "Message: " + message + "\n" + "Contact Method: " + contactMethod;
+
+            alert(confirmationMessage);
+          });
