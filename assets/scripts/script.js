392fe59a5b46e35547d1980c9d22c4c500d1bab0
Ross Espley <itzrossyo@gmail.com>
2023-06-14 12:40:53 +0100

    added jquery and js to product page for search of products

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,4 +6,83 @@ toggleBtn.onclick = function () {
   dropDownMenu.classList.toggle("open");
 };
 
+var val = $.trim(this.value);
+if (val === "")
+  $('img').show();
+else {
+  $('img').hide();
+  $("img[alt*=" + val + "]").show();
+}
 
+$(document).ready(function() {
+  $("#articleSearchInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#card *").filter(function() {
+
+      if (value == "beef") {
+        $("img").show();
+      }
+      if (value == "chi" || "chick") {
+        $("img").show();
+      }
+      if (value == "por" || "pork ") {
+        $("img").show();
+      }
+      if (value == "lam" || "lamb") {
+        $("img").show();
+      }
+      if (value == "tur" || "turkey") {
+        $("img").show();
+      }
+      if (value == "che" || "cheese") {
+        $("img").show();
+      }
+      if (value == "bac" || "bacon") {
+        $("img").show();
+      }
+      if (value == "sau" || "sausage") {
+        $("img").show();
+      }
+      
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+
+
+
+$(document).ready(function() {
+  $("#SearchInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#card *").filter(function() {
+
+      if (value == "beef") {
+        $("img").show();
+      }
+      if (value == "chi" || "chick") {
+        $("img").show();
+      }
+      if (value == "por" || "pork ") {
+        $("img").show();
+      }
+      if (value == "lam" || "lamb") {
+        $("img").show();
+      }
+      if (value == "tur" || "turkey") {
+        $("img").show();
+      }
+      if (value == "che" || "cheese") {
+        $("img").show();
+      }
+      if (value == "bac" || "bacon") {
+        $("img").show();
+      }
+      if (value == "sau" || "sausage") {
+        $("img").show();
+      }
+      
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
